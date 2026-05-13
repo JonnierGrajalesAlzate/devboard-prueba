c7c9bfff3d88eb3bc5a5bd1c227b315c27de0c52
JonnierGrajalesAlzate <alzatejonny39@gmail.com>
2026-05-12 21:03:59 -0500

    feat: implementacion crud iu

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -9,3 +9,32 @@ export const getTasks = async () => {
     return response.data;
 
 };
+
+export const createTask = async (taskData) => {
+
+    const response = await axios.post(API_URL, taskData);
+
+    return response.data;
+
+};
+
+export const updateTask = async (id, taskData) => {
+
+    const response = await axios.put(
+        `${API_URL}/${id}`,
+        taskData
+    );
+
+    return response.data;
+
+};
+
+export const deleteTask = async (id) => {
+
+    const response = await axios.delete(
+        `${API_URL}/${id}`
+    );
+
+    return response.data;
+
+};
